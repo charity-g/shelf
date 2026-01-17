d612bb6d846691fcfddffde7421bce552f656239
cherr <charity.grey1@gmail.com>
2026-01-17 14:27:02 -0800

    add in 3d rendering code

--- app/(tabs)/charity.tsx
+++ app/(tabs)/charity.tsx
@@ -1,37 +1,43 @@
-import { StyleSheet } from "react-native";
+import { GLView } from "expo-gl";
+import { Renderer } from "expo-three";
+import React, { Component } from "react";
+import * as THREE from "three";
 
+export default class Charity extends Component {
+  render() {
+    return (
+      <GLView style={{ flex: 1 }} onContextCreate={this._onGLContextCreate} />
+    );
+  }
+  _onGLContextCreate = async (gl: WebGLRenderingContext) => {
+    // 1. Scene
+    let scene = new THREE.Scene();
+    // 2. Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000,
+    );
+    // 3. Renderer
+    const renderer = new Renderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-import * as ImagePicker from 'expo-image-picker';
+    camera.position.z = 5;
 
-const PlaceholderImage = require("@/assets/images/background-image.png");
+    //scene.add(cube);
 
-export default function Index() {
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      console.log(result);
-    } else {
-      alert("You did not select any image.");
-    }
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    animate();
   };
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#25292e",
-      alignItems: "center",
-    },
-    imageContainer: {
-      flex: 1,
-    },
-    footerContainer: {
-      flex: 1 / 3,
-      alignItems: "center",
-    },
-  });
 }
