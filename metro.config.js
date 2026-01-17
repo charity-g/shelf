003ff3e20a7e88b06babcc3ac0094e2ecf179d32
cherr <charity.grey1@gmail.com>
2026-01-17 14:20:28 -0800

    update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
 const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push("glb", "gltf");
+
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  "js",
+  "jsx",
+  "json",
+  "ts",
+  "tsx",
+  "cjs",
+  "mjs",
+];
+config.resolver.assetExts = [
+  ...config.resolver.assetExts,
+  "glb",
+  "gltf",
+  "png",
+  "jpg",
+];
+
 module.exports = config;
