e8cda1486c6d9ccb086fe760e6a9af9b763f76b2
cherr <charity.grey1@gmail.com>
2026-01-17 14:17:27 -0800

    add packages and fix install

--- app/(tabs)/david.tsx
+++ app/(tabs)/david.tsx
@@ -1,16 +1,12 @@
-import { StyleSheet, View } from "react-native";
-
-import Button from "@/components/Button";
-import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { StyleSheet } from "react-native";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
-
-import * as ImagePicker from 'expo-image-picker';
 
 export default function Index() {
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ['images'],
+      mediaTypes: ["images"],
       allowsEditing: true,
       quality: 1,
     });
@@ -18,23 +14,22 @@ export default function Index() {
     if (!result.canceled) {
       console.log(result);
     } else {
-      alert('You did not select any image.');
+      alert("You did not select any image.");
     }
   };
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: "center",
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#25292e",
+      alignItems: "center",
+    },
+    imageContainer: {
+      flex: 1,
+    },
+    footerContainer: {
+      flex: 1 / 3,
+      alignItems: "center",
+    },
+  });
+}
