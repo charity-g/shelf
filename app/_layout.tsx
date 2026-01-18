bc3b3d6c758d7d04cd650117be352bdb889255f2
selina fu <selina.fu88@gmail.com>
2026-01-17 21:03:51 -0800

    remove nav bar from login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -99,6 +99,21 @@ export default function TabLayout() {
                   href: null,
               }}
           />    
+
+          <Tabs.Screen
+              name="login"
+              options={{
+                  href: null,
+              }}
+          />
+
+
+          <Tabs.Screen
+              name="signup"
+              options={{
+                  href: null,
+              }}
+          />
       </Tabs>
       
       
